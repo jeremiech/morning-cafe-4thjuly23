fb59cde9145e38043889a5baf1e0ad0e8fc6350b
Jeje <jeremiech06@gmail.com>
2023-06-29 15:02:29 +0200

    stage the the sum array element

---
+++ sumArray.js
@@ -0,0 +1,9 @@
+function sumArray(arr1,arr2){
+    let sum=0
+    for(let i=0;i<arr1.length;i++){
+        sum+=arr1[i]+arr2[i]
+        
+    }
+    return sum
+}
+console.log(sumArray([1,3,22],[11,8,4]))
